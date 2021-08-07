a9b80e730307d19c58d927fed7cd7400eb9e9b87
mstanka <mice78.mm@gmail.com>
2021-08-07 10:37:47 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <main class="font-spartan">
+  <the-header></the-header>
+  <section class="font-spartan max-w-screen-xl m-auto bg-neutralLighter h-screen">
     <h1>Frontend Mentor | Job Listings</h1>
-  </main>
+  </section>
 </template>
 
-<script></script>
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <img
+      src="../../assets/images/bg-header-mobile.svg"
+      alt=""
+      class="md:hidden max-w-screen-sm w-full m-auto bg-primary"
+    />
+    <img
+      src="../../assets/images/bg-header-desktop.svg"
+      alt=""
+      class="hidden md:block max-w-screen-xl m-auto bg-primary"
+    />
+  </header>
+</template>
