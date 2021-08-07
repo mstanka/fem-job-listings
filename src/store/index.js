ca92dbdf138f0980fbdbd9df6f00f60ccf0e065b
mstanka <mice78.mm@gmail.com>
2021-08-07 07:40:34 +0200

    create store

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import mutations from './mutations.js';
+import actions from './actions.js';
+import getters from './getters.js';
+
+const store = createStore({
+  state() {
+    return {
+      //....
+    }
+  },
+  mutations,
+  actions,
+  getters
+});
+
+export default store;
