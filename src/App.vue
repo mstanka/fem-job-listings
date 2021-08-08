1f2267e1b521748d1ab170a7a78104e8ddf701dc
mstanka <mice78.mm@gmail.com>
2021-08-08 09:39:22 +0200

    display data dynamically

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,42 @@
   <section
     class="font-spartan max-w-screen-xl m-auto bg-neutralLighter h-full py-10"
   >
-    <card-item></card-item>
-    <card-item></card-item>
-    <card-item></card-item>
+    <ul>
+      <card-item
+        v-for="job in jobs"
+        :key="job.id"
+        :id="job.id"
+        :company="job.company"
+        :logo="job.logo"
+        :isNew="job.new"
+        :featured="job.featured"
+        :position="job.position"
+        :role="job.role"
+        :level="job.level"
+        :postedAt="job.postedAt"
+        :contract="job.contract"
+        :location="job.location"
+        :languages="job.languages"
+        :tools="job.tools"
+      ></card-item>
+    </ul>
   </section>
 </template>
 
 <script>
 import TheHeader from './components/layout/TheHeader.vue';
 import CardItem from './components/card/CardItem.vue';
+import jobsData from './data.json';
 
 export default {
   components: {
     TheHeader,
     CardItem,
+  },
+  data() {
+    return {
+      jobs: jobsData,
+    };
   },
 };
 </script>
--- src/components/card/CardItem.vue
+++ src/components/card/CardItem.vue
@@ -1,42 +1,92 @@
 <template>
-  <div class="card">
-    <img
-      src="../../assets/images/eyecam-co.svg"
-      alt="logo"
-      class="absolute w-12 h-12 -top-6 left-6"
-    />
-    <div>
-      <div class="flex items-center">
-        <h3 class="text-sm">Photosnap</h3>
-        <button class="btn btn-primary" type="button">NEW</button>
-        <button class="btn btn-secondary" type="button">FEATURED</button>
-      </div>
-      <h2 class="text-neutralDarker font-bold py-2">
-        Senior Frontend Developer
-      </h2>
-      <div
+  <div class="card" :class="isFeaturedStyle">
+    <div class="md:flex md:flex-row md:items-center md:justify-between">
+      <img
+        :src="logo"
+        :alt="company"
         class="
-          text-neutralDark
-          flex
-          items-center
-          border-b border-neutralDark
-          pb-2
-          text-sm
+          absolute
+          md:relative
+          w-12
+          h-12
+          -top-6
+          md:top-0
+          left-6
+          md:left-0 md:mr-4
         "
-      >
-        <span>1d ago</span>
-        <span class="mx-1">&#183;</span>
-        <span>Full Time</span>
-        <span class="mx-1"> &#183;</span>
-        <span>USA only</span>
+      />
+      <div>
+        <div class="flex items-center mt-2 md:mt-0">
+          <h3 class="text-sm text-primary font-bold mr-2 md:mr-0">{{ company }}</h3>
+          <button class="btn btn-primary" type="button" v-if="isNew">
+            NEW!
+          </button>
+          <button class="btn btn-secondary" type="button" v-if="featured">
+            FEATURED
+          </button>
+        </div>
+        <h2 class="text-neutralDarker font-bold py-2">
+          {{ position }}
+        </h2>
+        <div
+          class="
+            text-neutralDark
+            flex
+            items-center
+            border-b border-neutralDark
+            md:border-none
+            pb-2 md:pb-0
+            text-sm
+          "
+        >
+          <span>{{ postedAt }}</span>
+          <span class="mx-1">&#183;</span>
+          <span>{{ contract }}</span>
+          <span class="mx-1"> &#183;</span>
+          <span>{{ location }}</span>
+        </div>
       </div>
     </div>
-    <div class="pt-2 flex flex-wrap items-center">
-      <button class="btn-label" type="button">Frontend</button>
-      <button class="btn-label" type="button">Senior</button>
-      <button class="btn-label" type="button">HMTL</button>
-      <button class="btn-label" type="button">CSS</button>
-      <button class="btn-label" type="button">Javascript</button>
+
+    <div class="pt-2 md:pt-0 flex flex-wrap items-center">
+      <button class="btn-label" type="button">{{ role }}</button>
+      <button class="btn-label" type="button">{{ level }}</button>
+      <button
+        v-for="language in languages"
+        :key="language"
+        class="btn-label"
+        type="button"
+      >
+        {{ language }}
+      </button>
+      <button v-for="tool in tools" :key="tool" class="btn-label" type="button">
+        {{ tool }}
+      </button>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    id: Number,
+    company: String,
+    logo: String,
+    isNew: Boolean,
+    featured: Boolean,
+    position: String,
+    role: String,
+    level: String,
+    postedAt: String,
+    contract: String,
+    location: String,
+    languages: Array,
+    tools: Array,
+  },
+  computed: {
+    isFeaturedStyle() {
+      return this.featured ? 'border-l-4 border-primary' : null;
+    },
+  },
+};
+</script>
